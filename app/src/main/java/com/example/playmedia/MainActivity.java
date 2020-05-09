1c025db499b5f7ed71bd638cd07349d7c30226dc
SULTAN <ramhziworld@gmail.com>
2020-05-09 07:49:42 +0100

    Added play button and music to play

--- app/src/main/java/com/example/playmedia/MainActivity.java
+++ app/src/main/java/com/example/playmedia/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.playmedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private Button playbButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.awade);
+
+
+        playbButton = findViewById(R.id.playButton);
+
+        playbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying())
+                {
+                    pauseMusic();
+
+                }
+                else
+                {
+                    playMusic();
+                }
+            }
+        });
+    }
+
+    public void pauseMusic()
+    {
+        if(mediaPlayer != null)
+        {
+            mediaPlayer.pause();
+            playbButton.setText(R.string.play_text);
+        }
+
+    }
+
+    public void playMusic()
+    {
+        if(mediaPlayer!=null) {
+        mediaPlayer.start();
+        playbButton.setText(R.string.pause_text);
+        }
+
     }
 }
