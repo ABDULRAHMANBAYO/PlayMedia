e682183805936ce33206ed703aa92a9f219f51dd
SULTAN <ramhziworld@gmail.com>
2020-05-09 08:10:46 +0100

    Refactor code and optimized play button

--- app/src/main/java/com/example/playmedia/MainActivity.java
+++ app/src/main/java/com/example/playmedia/MainActivity.java
@@ -7,49 +7,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
+//https://www.al-hamdoulillah.com/coran/mp3/files/mohammed-siddiq-minshawi/002.mp3
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
-    private Button playbButton;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = findViewById(R.id.playButton);
 
         mediaPlayer = new MediaPlayer();
-        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.awade);
+        try {
+            mediaPlayer.setDataSource("https://www.al-hamdoulillah.com/coran/mp3/files/mohammed-siddiq-minshawi/002.mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
-        playbButton = findViewById(R.id.playButton);
-
-        playbButton.setOnClickListener(new View.OnClickListener() {
+        MediaPlayer.OnPreparedListener prepareListener = new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onClick(View v) {
-                if (mediaPlayer.isPlaying()) {
-                    pauseMusic();
+            public void onPrepared(final MediaPlayer mp) {
 
-                } else {
-                    playMusic();
-                }
+                playButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mp.isPlaying()
+                        ) {
+                            mp.pause();
+                            playButton.setText(R.string.play_text);
+                        }
+
+                     else
+
+                        {
+                            mp.start();
+                            playButton.setText(R.string.pause_text);
+
+                        }
+                    }
+                });
+
             }
-        });
-    }
+        };
+        mediaPlayer.setOnPreparedListener(prepareListener);
 
-    public void pauseMusic() {
-        if (mediaPlayer != null) {
-            mediaPlayer.pause();
-            playbButton.setText(R.string.play_text);
-        }
+        mediaPlayer.prepareAsync();
+//        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.awade);
 
     }
 
-    public void playMusic() {
-        if (mediaPlayer != null) {
-            mediaPlayer.start();
-            playbButton.setText(R.string.pause_text);
-        }
-
-    }
+//    public void pauseMusic() {
+//        if (mediaPlayer != null) {
+//            mediaPlayer.pause();
+//            playButton.setText(R.string.play_text);
+//        }
+//
+//    }
+//
+//    public void playMusic() {
+//        if (mediaPlayer != null) {
+//            mediaPlayer.start();
+//            playButton.setText(R.string.pause_text);
+//        }
+//
+//    }
 
     @Override
     protected void onDestroy() {
